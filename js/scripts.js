9241425a7c2eedea2c82fac2cd02f6750425047b
Matt Botz <botzmatt@yahoo.com>
2016-06-01 18:53:41 -0700

    add index.html structure for supporting the scripts.js functions that are written. Adds ability to display users calculations directly on the webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,25 @@
-var add = function(number1, number2){
+var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2);
-
-var subtract = function(number3, number4){
-  return number3 - number4;
+var subtract = function(number1, number2) {
+  return number1 - number2;
 };
 
-var number3 = parseInt(prompt("Enter a number:"));
-var number4 = parseInt(prompt("Enter another number:"));
-
-alert(subtract(number3, number4);
-
-var divide = function(number5, number6){
-  return number5 / number6;
+var multiply = function(number1, number2) {
+  return number1 * number2;
 };
 
-var number5 = parseInt(prompt("Enter a number:"));
-var number6 = parseInt(prompt("Enter another number:"));
-
-alert(divide(number5, number6);
-
-var multiply = function(number7, number8){
-  return number7 * number8;
+var divide = function(number1, number2) {
+  return number1 / number2;
 };
 
-var number7 = parseInt(prompt("Enter a number:"));
-var number8 = parseInt(prompt("Enter another number:"));
-
-alert(multiply(number7, number8);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
