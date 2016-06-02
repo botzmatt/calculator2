609c7be45991c27ed6f1e1c61a3e68c025669315
Matt Botz <botzmatt@yahoo.com>
2016-06-01 18:35:21 -0700

    add functions for addition, subtraction, multiplication, and division to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+var add = function(number1, number2){
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1, number2);
+
+var subtract = function(number3, number4){
+  return number3 - number4;
+};
+
+var number3 = parseInt(prompt("Enter a number:"));
+var number4 = parseInt(prompt("Enter another number:"));
+
+alert(subtract(number3, number4);
+
+var divide = function(number5, number6){
+  return number5 / number6;
+};
+
+var number5 = parseInt(prompt("Enter a number:"));
+var number6 = parseInt(prompt("Enter another number:"));
+
+alert(divide(number5, number6);
+
+var multiply = function(number7, number8){
+  return number7 * number8;
+};
+
+var number7 = parseInt(prompt("Enter a number:"));
+var number8 = parseInt(prompt("Enter another number:"));
+
+alert(multiply(number7, number8);
